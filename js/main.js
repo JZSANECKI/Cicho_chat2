e29ff92e52b9c83cc3a5bbff815c1c5432d92801
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-06 21:41:18 +0100

    9 files modified

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const nav = document.querySelector('.navbar')
-  const allNavItems = document.querySelectorAll('.nav-link')
-  const navList = document.querySelector('.navbar-collapse')
+  const nav = document.querySelector('.navbar');
+  const allNavItems = document.querySelectorAll('.nav-link');
+  const navList = document.querySelector('.navbar-collapse');
 
-  
-
-  allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
-
- 
-})
+  allNavItems.forEach((item) =>
+    item.addEventListener('click', () => navList.classList.remove('show'))
+  );
+});
